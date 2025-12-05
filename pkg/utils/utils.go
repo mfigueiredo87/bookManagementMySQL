4dd2f4328c12bb47024b142bd3497e1eedca9183
mfigueiredo87 <man.figueiredo@hotmail.com>
2025-12-05 07:57:00 +0000

    util

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+
+func ParseBody(r *http.Request, v interface{}) error {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), v); err != nil {	
+			return err
+		}
+	}
+	return nil
+}
